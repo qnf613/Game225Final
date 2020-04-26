67d1cd718a0ef96259fe927fc0cc441ec4f69715
qnf613 <gurwls613@gmail.com>
2020-04-26 12:42:54 -0500

    tempMainMenuSetUp+PlayerControl

---
+++ FinalProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D playerRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float xVelocity = horizontalInput * speed;
+        playerRigidbody.velocity = new Vector2(xVelocity, playerRigidbody.velocity.y);
+    }
+}
