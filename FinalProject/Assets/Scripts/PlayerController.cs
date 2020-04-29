5818a06091a7d13e9505816971208b3414f80e85
qnf613 <gurwls613@gmail.com>
2020-04-29 17:58:30 -0500

    PlyayerController

--- FinalProject/Assets/Scripts/PlayerController.cs
+++ FinalProject/Assets/Scripts/PlayerController.cs
@@ -5,18 +5,57 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed;
-    private Rigidbody2D playerRigidbody;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private int jumpcount = 0;
+    private Rigidbody2D rigid;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        playerRigidbody = GetComponent<Rigidbody2D>();
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //movement speed
         float horizontalInput = Input.GetAxis("Horizontal");
         float xVelocity = horizontalInput * speed;
-        playerRigidbody.velocity = new Vector2(xVelocity, playerRigidbody.velocity.y);
+        rigid.velocity = new Vector2(xVelocity, rigid.velocity.y);
+        //stop speed
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
+        //jump & double jump
+        if (Input.GetButtonDown("Jump") && jumpcount < 2)
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            
+            if (jumpcount > 0 && jumpcount < 2) 
+            {
+                rigid.velocity = Vector2.zero;
+                rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            }
+            jumpcount++;
+
+        }
+        
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 0.1f);
+        if (rayHit.collider != null)
+        {
+            if (collision.gameObject.tag =="Ground")
+            {
+                jumpcount = 0;
+            }
+        }
+    }
+
 }
