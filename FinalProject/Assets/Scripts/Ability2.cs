5f0a8eebf3003fbf0c2b350dc5a06de86023d9ec
qnf613 <gurwls613@gmail.com>
2020-05-01 20:20:25 -0500

    Ability 2

--- FinalProject/Assets/Scripts/Ability2.cs
+++ FinalProject/Assets/Scripts/Ability2.cs
@@ -5,27 +5,42 @@ using UnityEngine;
 public class Ability2 : MonoBehaviour
 {
     [SerializeField] public bool AB2usable = false;
+    GameObject target;
+    Vector2 targetPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButton(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit) && gameObject.CompareTag("MovableOb"))
+            OjDetect();
+            if (target.CompareTag("MovableOb"))
             {
-                Debug.Log("Hit");
+                Debug.Log("Movable");
+                target.transform.position = targetPos;
             }
+
         }
 
     }
 
+    void OjDetect()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+
+        if (hit.collider != null)
+        {
+            target = hit.collider.gameObject;
+            Debug.Log("Hit");
+        }
+    }
 
 
 }
